f9827d3662eca8afa168893e98f66ed7432fda64
Mohammad ahmad <140886835+Mohammad-Ahmad003@users.noreply.github.com>
2025-07-05 11:22:57 +0530

    Time: 1 ms (99.94%), Space: 43.4 MB (43.8%) - LeetHub

--- 1510-find-lucky-integer-in-an-array/1510-find-lucky-integer-in-an-array.java
+++ 1510-find-lucky-integer-in-an-array/1510-find-lucky-integer-in-an-array.java
@@ -1,21 +1,35 @@
 class Solution {
     public int findLucky(int[] arr) {
-        //int freq[]= new int[501];
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int num:arr){
-            {
-                map.put(num,map.getOrDefault(num,0)+1);
-            }
-        }
-        int luckyint=-1;
-        for(Map.Entry<Integer,Integer> entry: map.entrySet()){
-            int value=entry.getValue();
-            int key = entry.getKey();
+        int freq[]= new int[501];
+        int len=arr.length;
+        // HashMap<Integer,Integer> map = new HashMap<>();
+        // for(int num:arr){
+        //     {
+        //         map.put(num,map.getOrDefault(num,0)+1);
+        //     }
+        // }
+        // int luckyint=-1;
+        // for(Map.Entry<Integer,Integer> entry: map.entrySet()){
+        //     int value=entry.getValue();
+        //     int key = entry.getKey();
 
-            if(key==value){
-                luckyint=Math.max(luckyint,value);
+        //     if(key==value){
+        //         luckyint=Math.max(luckyint,value);
+        //     }
+        // }
+        for(int i=0;i<len;i++){
+            freq[arr[i]]++;
+        }
+        int maxfreq=-1;
+        for(int i=1;i<501;i++){
+            if(freq[i]==i){
+                maxfreq=Math.max(maxfreq,i);
             }
         }
-        return luckyint;
+
+
+        // return luckyint;
+
+        return maxfreq;
     }
 }
